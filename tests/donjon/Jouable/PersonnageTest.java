b6807da50135da93624a0056ff60160ef9483e54
maxenceflr <maxenceflieller10@gmail.com>
2025-05-31 17:36:03 +0200

    zfea

---
+++ tests/donjon/Jouable/PersonnageTest.java
@@ -0,0 +1,50 @@
+package donjon.Jouable;
+
+import jouable.Monstre;
+import jouable.personnage.Personnage;
+import jouable.personnage.classe.Guerrier;
+import jouable.personnage.race.Humain;
+import objet.arme.*;
+import objet.armure.DemiPlate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import stats.CaracteristiquesBase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonnageTest {
+    private Personnage personnage;
+
+    @BeforeEach
+    void setUp() {
+        CaracteristiquesBase stats = new CaracteristiquesBase(10, 10, 10, 10, 10, 10);
+        personnage = new Personnage("Maxence", new Humain(), new Guerrier(), stats);
+    }
+
+    @Test
+    void testNomEtClasse() {
+        assertEquals("Maxence", personnage.getNom());
+        assertEquals("Guerrier", personnage.getClasse().toString());
+    }
+
+    @Test
+    void testEquiperArmeEtArmure() {
+        Baton baton = new Baton();
+        DemiPlate demi = new DemiPlate();
+
+        personnage.setArme(baton);
+        personnage.setArmure(demi);
+
+        assertEquals(personnage.getArme(),baton);
+        assertEquals(personnage.getArmure(),demi);
+    }
+
+    @Test
+    void testSymboleNom() {
+        assertEquals("Max", personnage.getSymbole());
+    }
+
+
+}
+
+
