e321a59ffef414c39868d4017ac5a577dcf28320
Maxime Malys <malys@unistra.fr>
2025-06-05 11:02:27 +0200

    oui

--- src/partie/Partie.java
+++ src/partie/Partie.java
@@ -1,8 +1,20 @@
 package partie;
 
+import donjon.Donjon;
+
 public class Partie {
 
     private Tour m_tour;
+    private Donjon m_donjonEnCours;
 
+    public Partie(Donjon donjonEnCours)
+    {
+        m_tour = new Tour();
+        m_donjonEnCours = donjonEnCours;
+    }
 
+    public void InitialiserPartie()
+    {
+
+    }
 }
