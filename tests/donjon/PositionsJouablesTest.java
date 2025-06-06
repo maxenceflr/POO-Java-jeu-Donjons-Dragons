5fd718d12b7156d72fa520a38df0211084e5b0ba
Maxime Malys <malys@unistra.fr>
2025-06-06 22:35:25 +0200

    Test_Affichage_Code

--- tests/donjon/PositionsJouablesTest.java
+++ tests/donjon/PositionsJouablesTest.java
@@ -1,5 +1,6 @@
 package donjon;
 
+import affichage.AffichageDonjon;
 import jouable.personnage.Personnage;
 import jouable.personnage.classe.Guerrier;
 import jouable.personnage.race.Humain;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import stats.CaracteristiquesBase;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PositionsJouablesTest {
 
@@ -17,13 +19,28 @@ class PositionsJouablesTest {
         CaracteristiquesBase stats = new CaracteristiquesBase(10, 10, 10, 10, 10, 10);
         Personnage personnageTest= new Personnage("Maxence", new Humain(), new Guerrier(), stats);
         Position position1 = new Position(10,10);
-        Position position2 = new Position(5, 5);
+        Position position2 = new Position(11, 11);
 
         Donjon donjonTest= new Donjon();
         donjonTest.getPositionsJouables().ajouterJouable(personnageTest, position1);
         donjonTest.getPositionsJouables().deplacerJouable(personnageTest, position2, donjonTest);
 
+        System.out.println(donjonTest.getPositionsJouables().toString());
         assertEquals(position2, donjonTest.getPositionFromJouable(personnageTest));
-
     }
+
+    @Test
+    void DeplacementCode()
+    {
+        CaracteristiquesBase stats = new CaracteristiquesBase(10, 10, 10, 10, 10, 10);
+        Personnage personnageTest= new Personnage("Maxence", new Humain(), new Guerrier(), stats);
+        Donjon donjonTest= new Donjon();
+        String code = "A1";
+
+        Position position1 = new Position(Position.getPositionFromCode(code));
+        donjonTest.getPositionsJouables().ajouterJouable(personnageTest, position1);
+
+        AffichageDonjon.afficherDonjon(donjonTest);
+    }
+
 }
